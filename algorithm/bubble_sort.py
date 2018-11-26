8a134951c8fe76c37e694ca6047c907fed35c5de
PENGYUXIONG <pxiong@ualberta.ca>
2018-11-25 22:15:32 -0600

    optimize bubble_sort

--- algorithm/bubble_sort.py
+++ algorithm/bubble_sort.py
@@ -1,15 +1,16 @@
 def bubble_sort(sample_array):
 	swap_boolean = False
 
-	for prev_pos in range(len(sample_array)-1):
-		cur_pos = prev_pos + 1
-		if sample_array[prev_pos] > sample_array[cur_pos]:
-			sample_array[prev_pos], sample_array[cur_pos] = sample_array[cur_pos], sample_array[prev_pos]
-			swap_boolean = True
-	if not swap_boolean:
-		return sample_array
+	for i in range(len(sample_array)):
+		for prev_pos in range(len(sample_array)-i-1):
+			cur_pos = prev_pos + 1
+			if sample_array[prev_pos] > sample_array[cur_pos]:
+				sample_array[prev_pos], sample_array[cur_pos] = sample_array[cur_pos], sample_array[prev_pos]
+				swap_boolean = True
+		if not swap_boolean:
+			break
 	
-	return bubble_sort(sample_array)	
+	return sample_array
 
 
 sample_array = [4, 1, 2, -5, -7, -3, 0]
